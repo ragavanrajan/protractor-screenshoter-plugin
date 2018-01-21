fb62898e0375825aad54c3ff82ddbda2e176d2ff
azachar <andrej@chocolatejar.eu>
2018-01-21 10:51:45 +0100

    test(protractor): logs in headless mode

--- spec/integrational/protractor-config/failuresWithLogs.js
+++ spec/integrational/protractor-config/failuresWithLogs.js
@@ -5,7 +5,22 @@ exports.config = {
   framework: 'jasmine2',
   specs: ['../protractor/fail-test-with-console-errors.js'],
   capabilities: {
-    browserName: env.capabilities.browserName,
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [
+        '--use-fake-device-for-media-stream',
+        '--use-fake-ui-for-media-stream',
+        '--verbose',
+        '--enable-logging',
+        '--no-sandbox',
+        '--disable-gpu',
+        '--headless'
+      ],
+      // disable Password manager popup
+      prefs: {
+        'credentials_enable_service': false
+      }
+    },
     loggingPrefs: {
       browser: 'ALL' // "OFF", "SEVERE", "WARNING", "INFO", "CONFIG", "FINE", "FINER", "FINEST", "ALL".
     }
